47783a254f00250e7c4205eee7df5b25075e653f
flsf <jianmaflsf@gmail.com>
2015-09-09 17:22:16 +0800

    Change version to 1.0.0dev

--- pocsuite/lib/core/settings.py
+++ pocsuite/lib/core/settings.py
@@ -13,7 +13,7 @@ import sys
 
 from lib.core.revision import getRevisionNumber
 
-VERSION = "0.3"
+VERSION = "1.0.0dev"
 REVISION = getRevisionNumber()
 SITE = "http://sebug.net"
 VERSION_STRING = "pocsuite/%s%s" % (VERSION, "-%s" % REVISION if REVISION else "-nongit-%s" % time.strftime("%Y%m%d", time.gmtime(os.path.getctime(__file__))))
