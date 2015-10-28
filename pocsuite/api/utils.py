e465dd6be1e3f261ad86082a80cb25216ec9fea6
flsf <jianmaflsf@gmail.com>
2015-10-28 17:20:50 +0800

    Add api/utils - password

--- pocsuite/api/utils.py
+++ pocsuite/api/utils.py
@@ -8,3 +8,7 @@ See the file 'docs/COPYING' for copying permission
 
 from pocsuite.lib.core.data import logger
 from pocsuite.lib.core.enums import CUSTOM_LOGGING
+
+from pocsuite.lib.utils.password import getLargeWeakPassword
+from pocsuite.lib.utils.password import getWeakPassword
+from pocsuite.lib.utils.password import genPassword
