c0197bcf029f95bf3d1df8249d52fec62fee0592
flsf <jianmaflsf@gmail.com>
2015-10-30 13:45:04 +0800

    Compatible python 2.6

--- pocsuite/lib/parse/parser.py
+++ pocsuite/lib/parse/parser.py
@@ -6,11 +6,10 @@ Copyright (c) 2014-2015 pocsuite developers (http://sebug.net)
 See the file 'docs/COPYING' for copying permission
 """
 
-import argparse
 import os
 from pocsuite.lib.core.data import defaults
 from pocsuite.lib.core.settings import INDENT, USAGE, VERSION
-
+from pocsuite.thirdparty.argparse import argparse
 
 def parseCmdOptions():
     """
