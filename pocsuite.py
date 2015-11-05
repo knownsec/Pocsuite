763fade500a171a147fd7e589de33e6bf3ffbfcb
flsf <jianmaflsf@gmail.com>
2015-11-05 17:04:22 +0800

    Minor change

--- pocsuite.py
+++ pocsuite.py
@@ -8,7 +8,7 @@ See the file 'docs/COPYING' for copying permission
 import re
 import sys
 from pocsuite.lib.utils import versioncheck
-from pocsuite.pocsuite import main
+from pocsuite.pocsuite_cli import main
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
