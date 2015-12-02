4a5be95224bcae1af15e31ad6bfb78582882f26b
Wyc0 <wuyc@knownsec.com>
2015-12-02 14:06:52 +0800

    update all params in missile_info

--- pocsuite/api/missile.py
+++ pocsuite/api/missile.py
@@ -6,25 +6,18 @@ Copyright (c) 2014-2015 pocsuite developers (http://sebug.net)
 See the file 'docs/COPYING' for copying permission
 """
 
-from pocsuite.pocsuite_cli import pcsInit
-from pocsuite.lib.core.settings import PCS_OPTIONS
-from pocsuite.lib.core.common import banner
 from pocsuite.lib.core.data import kb
+from pocsuite.pocsuite_cli import pcsInit
+from pocsuite.lib.core.common import banner
+from pocsuite.lib.core.settings import PCS_OPTIONS
+from pocsuite.lib.settings import HTTP_DEFAULT_HEADER
 
 
 class Missile():
     def __init__(self, target, missile_info={}):
         if not missile_info["pocname"].endswith(".py"):
             missile_info["pocname"] += ".py"
-        PCS_OPTIONS.update({
-            "url": target,
-            "pocFile": missile_info["pocstring"],
-            "isPocString": True,
-            "pocname": missile_info["pocname"],
-            "headers": "",
-            "extra_params": "",
-            "mode": missile_info["mode"],
-        })
+        PCS_OPTIONS.update(missile_info)
 
     def run(self):
         pcsInit(PCS_OPTIONS)
