add1422dd9b2c5b90e7f7fd55fb97879f326a7b3
Wyc0 <wuyc@knownsec.com>
2015-12-09 14:00:15 +0800

    handler given param --url like 1.1.1.1/24

--- pocsuite/lib/core/option.py
+++ pocsuite/lib/core/option.py
@@ -200,7 +200,21 @@ def setMultipleTarget():
 
     if not conf.urlFile:
         for pocname, pocInstance in kb.registeredPocs.items():
-            kb.targets.put((conf.url, pocInstance, pocname))
+            if conf.url.endswith('/24'):
+                try:
+                    socket.inet_aton(conf.url.split('/')[0])
+                    base_addr = conf.url[:conf.url.rfind('.') + 1]
+                    conf.url = ['{}{}'.format(base_addr, i) \
+                                        for i in xrange(1, 255 + 1)]
+                except socket.error:
+                    errMsg = 'only id address acceptable'
+                    logger.log(CUSTOM_LOGGING.ERROR, errMsg)
+            else:
+                conf.url = conf.url.split(',')
+        for url in conf.url:
+            if url:
+                kb.targets.put((url, pocInstance, pocname))
+
         return
 
     conf.urlFile = safeExpandUser(conf.urlFile)
