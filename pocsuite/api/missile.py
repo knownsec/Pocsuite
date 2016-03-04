e5da641ee3c449afff3a4c7d00310e90503ebddd
flsf <jianmaflsf@gmail.com>
2016-03-04 17:39:44 +0800

    Update missile

--- pocsuite/api/missile.py
+++ pocsuite/api/missile.py
@@ -6,40 +6,46 @@ Copyright (c) 2014-2015 pocsuite developers (http://seebug.org)
 See the file 'docs/COPYING' for copying permission
 """
 
+import time
 from pocsuite.lib.core.data import kb
-from pocsuite.pocsuite_cli import pcsInit
+from pocsuite.lib.core.data import conf
+from pocsuite.lib.core.common import filepathParser
+from pocsuite.lib.core.common import multipleReplace
+from pocsuite.lib.core.common import StringImporter
+from pocsuite.lib.core.settings import POC_IMPORTDICT
+from pocsuite.lib.core.settings import HTTP_DEFAULT_HEADER
+
 
 class Missile():
 
     def __init__(self, target, missile_info={}):
-        if not missile_info["pocname"].endswith(".py"):
-            missile_info["pocname"] += ".py"
-        self.PCS_OPTIONS = {
-            "url": target,
-            "host": "",
-            "pocFile": missile_info["pocstring"],
-            "isPocString": True,
-            "pocname": missile_info["pocname"],
-            "headers": "",
-            "extra_params": "",
-            "Mode": missile_info["mode"],
-            "retry": False,
-            "delay": 0,
-            "requires": False,
-            "requiresFreeze": False,
-            "quiet": True,
-            'threads': 1,
-            'urlFile': None,
-            'agent': None,
-            'referer': None,
-            'cookie': None,
-            'randomAgent': False,
-            'report': None,
-            'proxy': None,
-            'proxyCred': None,
-            'timeout': 5
-        }
+        self.target = target
+        self.pocString = missile_info["pocstring"]
+        self.pocName = missile_info["pocname"]
+        self.mode = "verify"
+        self.params = {}
+        conf.isPycFile = False
+        conf.httpHeaders = {}
+
+        try:
+            kb.registeredPocs
+        except Exception:
+            kb.registeredPocs = {}
+
+        self.registerPoc()
+
+    def registerPoc(self):
+        pocString = multipleReplace(self.pocString, POC_IMPORTDICT)
+        _, self.moduleName = filepathParser(self.pocName)
+        try:
+            importer = StringImporter(self.moduleName, pocString)
+            importer.load_module(self.moduleName)
+        except ImportError, ex:
+            pass  # TODO
 
     def run(self):
-        pcsInit(self.PCS_OPTIONS)
-        return kb.results
+        poc = kb.registeredPocs[self.moduleName]
+        result = poc.execute(self.target, mode=self.mode)
+        output = (self.target, self.pocName, result.vulID, result.appName, result.appVersion, "success" if result.is_success() else "failed", time.strftime("%Y-%m-%d %X", time.localtime()), str(result.result))
+
+        return output
